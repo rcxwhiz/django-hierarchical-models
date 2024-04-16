7101d07b740073dd5268f4307877b2c7407aa78a
Josh Bedwell <rcxwhiz@gmail.com>
2024-04-16 09:33:33 -0500

    Got test model fields moved out to a mixin

--- tests/models.py
+++ tests/models.py
@@ -3,6 +3,13 @@ from django.db import models
 from django_hierarchical_models.models import AdjacencyListModel
 
 
-class ALMPerson(AdjacencyListModel):
+class PersonModelMixin(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+
+    class Meta:
+        abstract = True
+
+
+class ALMPerson(PersonModelMixin, AdjacencyListModel):
+    pass

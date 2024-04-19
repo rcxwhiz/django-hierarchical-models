751da6f51371dc394dceaca6329fc1cd14b22099
Josh Bedwell <rcxwhiz@gmail.com>
2024-04-19 18:40:30 -0500

    Added an exception for cycles

--- django_hierarchical_models/models/exceptions.py
+++ django_hierarchical_models/models/exceptions.py
@@ -15,3 +15,13 @@ class NotAChildException(Exception):
 
     def __str__(self):
         return f"{self.child} is not a child of {self.parent}"
+
+
+class CycleException(Exception):
+    def __init__(self, parent, child, *args):
+        super().__init__(*args)
+        self.parent = parent
+        self.child = child
+
+    def __str__(self):
+        return f"Making {self.child} a child of {self.parent} would create a cycle"

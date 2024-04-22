5972439cd08cdc38173622b35dbea55393d69206
Josh Bedwell <rcxwhiz@gmail.com>
2024-04-21 22:53:19 -0500

    Added detection to the PEM test for sqlite etc.

--- tests/interface_test.py
+++ tests/interface_test.py
@@ -1,4 +1,4 @@
-import pytest
+from django.conf import settings
 from django.test import TestCase
 
 from tests.interface_tester import HierarchicalModelInterfaceTester
@@ -13,6 +13,16 @@ class NSMInterfaceTest(HierarchicalModelInterfaceTester, TestCase):
     model_class = NSMTestModel
 
 
-@pytest.mark.skip("Needs non-sqlite")
 class PEMInterfaceTest(HierarchicalModelInterfaceTester, TestCase):
     model_class = PEMTestModel
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        db_engine = settings.DATABASES["default"]["ENGINE"]
+
+        if "sqlite" in db_engine or "oracle" in db_engine:
+            self.__class__.__unittest_skip__ = True
+            self.__class__.__unittest_skip_why__ = (
+                "PathEnumerationModel tests skipped due to sqlite or oracle db engine."
+            )

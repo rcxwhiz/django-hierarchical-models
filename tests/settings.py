294e5a7007c3511c0fcc9f2b397e3f81f492c242
Josh Bedwell <rcxwhiz@gmail.com>
2024-04-21 23:04:05 -0500

    Added postgres to GH actions tests

--- tests/settings.py
+++ tests/settings.py
@@ -7,13 +7,13 @@ INSTALLED_APPS = (
     "tests",
 )
 
-if os.environ.get("POSTGRES"):
+if os.environ.get("POSTGRES_DB"):
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.environ.get("POSTGRES_DB", "dhm_db"),
-            "USER": os.environ.get("POSTGRES_USER", "dhm_user"),
-            "PASSWORD": os.environ.get("POSTGRES_PASSWORD", ""),
+            "NAME": os.environ.get("POSTGRES_DB", "postgres"),
+            "USER": os.environ.get("POSTGRES_USER", "postgres"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "postgres"),
             "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
             "PORT": os.environ.get("POSTGRES_PORT", "5432"),
         },

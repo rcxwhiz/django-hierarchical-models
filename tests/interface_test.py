89a26ef61f9204d27cb894991f02084c8ded3932
Josh Bedwell <rcxwhiz@gmail.com>
2024-04-22 08:09:33 -0500

    Made postgres tests not skip when environment variables set

--- tests/interface_test.py
+++ tests/interface_test.py
@@ -1,3 +1,5 @@
+import os
+
 from django.conf import settings
 from django.test import TestCase
 
@@ -19,10 +21,13 @@ class PEMInterfaceTest(HierarchicalModelInterfaceTester, TestCase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        db_engine = settings.DATABASES["default"]["ENGINE"]
+        # don't want to skip tests if intended to use postgres
+        if "POSTGRES_DB" not in os.environ:
+            db_engine = settings.DATABASES["default"]["ENGINE"]
 
-        if "sqlite" in db_engine or "oracle" in db_engine:
-            self.__class__.__unittest_skip__ = True
-            self.__class__.__unittest_skip_why__ = (
-                "PathEnumerationModel tests skipped due to sqlite or oracle db engine."
-            )
+            if "sqlite" in db_engine or "oracle" in db_engine:
+                self.__class__.__unittest_skip__ = True
+                self.__class__.__unittest_skip_why__ = (
+                    "PathEnumerationModel tests skipped due to sqlite or oracle db"
+                    "engine."
+                )
